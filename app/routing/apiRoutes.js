2d938128891fc2d2950231842303eb29b54ecf65
sam-pop <poplovitch.samuel@gmail.com>
2018-06-08 23:34:54 -0400

    router update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,5 +3,15 @@ const friends = require('../data/friends');
 
 module.exports = function (app) {
 
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
+
+    app.post('api/friends', function (req, res) {
+        if (req.body) {
+            //TODO: check for compatibility (using `req.body` against the `friends` array)
+            //TODO: return the most compatible friend (using `res`)
+        }
+    });
 
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,6 +8,7 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
 
+    // default route
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
