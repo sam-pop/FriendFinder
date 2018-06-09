6b087e8c94a135b31166028eda7476138d80147f
sam-pop <poplovitch.samuel@gmail.com>
2018-06-09 11:29:34 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,11 +7,32 @@ module.exports = function (app) {
         res.json(friends);
     });
 
-    app.post('api/friends', function (req, res) {
+    app.post('/api/friends', function (req, res) {
         if (req.body) { //TODO: check that this is the right condition
             //TODO: check for compatibility (using `req.body` against the `friends` array)
             //TODO: return the most compatible friend (using `res`)
+            res.json(checkCompatibility(req.body));
         }
     });
 
 };
+
+
+function checkCompatibility(arr) {
+    let bestScore = Infinity;
+    let bestFriend;
+    for (let friend of friends) {
+        let totalDifference = 0;
+        for (let i = 0; i < friend.scores.length; i++) {
+            totalDifference += Math.abs(friend.scores[i] - arr[i]);
+        }
+        if (totalDifference < bestScore) {
+            bestScore = totalDifference;
+            bestFriend = friend;
+        }
+    }
+    console.log('​checkCompatibility -> bestScore', bestScore);
+
+    console.log('​checkCompatibility -> bestFriend', bestFriend);
+    return bestFriend;
+}
