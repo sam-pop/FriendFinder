4da7980d6616ad9dad1b949c49595eea0cacc938
sam-pop <poplovitch.samuel@gmail.com>
2018-06-08 22:50:09 -0400

    frieds test data

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [{
+    "name": "Tester McTesty",
+    "photo": "https://randomuser.me/api/portraits/men/0.jpg",
+    "scores": [5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}];
+
+module.exports = friends;
