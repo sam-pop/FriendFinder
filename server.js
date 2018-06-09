37dc0484c47b586d5869fd056c93cecfa4bfdf91
sam-pop <poplovitch.samuel@gmail.com>
2018-06-08 20:35:17 -0400

    server listen

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const path = require('path');
 // Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('hello world!');
+});
+
+
+// Server listen
+app.listen(3000, function () {
+    console.log("Server running on Port 3000...");
+});
