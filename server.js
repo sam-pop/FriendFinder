f373f7d78d2421e710ef5304e263f6779ae410bc
sam-pop <poplovitch.samuel@gmail.com>
2018-06-08 20:42:47 -0400

    body-parser middleware

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
 app.get('/', function (req, res) {
     res.send('hello world!');
 });
-
 
 // Server listen
 app.listen(3000, function () {
