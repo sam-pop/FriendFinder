1bd2c05b38fb12ac3df7fc852ef5ccd5146feb49
sam-pop <poplovitch.samuel@gmail.com>
2018-06-09 11:57:56 -0400

    routes comments update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,10 +3,12 @@ const friends = require('../data/friends');
 
 module.exports = function (app) {
 
+    // display a JSON of all possible friends.
     app.get('/api/friends', function (req, res) {
         res.json(friends);
     });
 
+    //  handles incoming survey results
     app.post('/api/friends', function (req, res) {
         if (req.body) {
             res.json(checkCompatibility(req.body));
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,13 +4,14 @@ const path = require('path');
 
 module.exports = function (app) {
 
+    // survey page route
     app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
 
     // default route
-    // app.get('*', function (req, res) {
-    //     res.sendFile(path.join(__dirname, '../public/home.html'));
-    // });
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
 
 };
