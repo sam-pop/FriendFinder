410a31af9cc830b204f6c41da4bdfac54cded3fa
sam-pop <poplovitch.samuel@gmail.com>
2018-06-08 20:28:13 -0400

    first

--- server.js
+++ server.js
@@ -2,3 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+// Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
